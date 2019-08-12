12a48ad1d12ce14cad982c8658d9a95f3c88b508
Chandan Sharma <chandan.cmd@gmail.com>
2019-08-12 23:39:13 +0530

    Add files via upload

---
+++ basic/basic.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.22;
+contract smartcontract1{
+	mapping (address => uint256) public balanceOf;
+	// balanceOf[address] = 5;
+
+	constructor(uint256 initialSupply) public {
+		balanceOf[msg.sender] = initialSupply;
+	}
+
+	function transfer(address _to, uint256 _value) public {
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+	}
+}
